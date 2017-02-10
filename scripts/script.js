dc072da4e4943e7ae49cfaba8abb039132ff6f09
Adam Lezotte <adam@lezotte.me>
2017-02-09 20:45:30 -0500

    put code in function

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,28 @@
-var temp1 = prompt("Enter a temperature:");
-var unit1 = prompt("Enter 'F for Fahrenheit or 'C' for Celsius:").toUpperCase();
-var temp2;
-var unit2;
-
-if (unit1 === "F") {
-    unit2 = "C";
-} else if (unit1 === "C") {
-    unit2 = "F";
+function tempConvert(temp1, unit1) {
+    unit1 = unit1.toUpperCase();
+    
+    var temp2;
+    var unit2;
+    
+    if (unit1 === "F") {
+        unit2 = "C";
+    } else if (unit1 === "C") {
+        unit2 = "F";
+    }
+    
+    if (unit1 === "F") {
+        temp2 = (temp1 - 32) * 5 / 9;
+        temp2 = Math.round(temp2 * 10) / 10;
+        console.log(temp1 + "\u00B0" + unit1 + " is " + temp2 + "\u00B0" + unit2 + ".");
+    } else if (unit1 === "C") {
+        temp2 = temp1 * 9 / 5 + 32;
+        temp2 = Math.round(temp2 * 10) / 10;
+        console.log(temp1 + "\u00B0" + unit1 + " is " + temp2 + "\u00B0" + unit2 + ".");
+    }
 }
 
-if (unit1 === "F") {
-    temp2 = (temp1 - 32) * 5 / 9;
-    temp2 = Math.round(temp2 * 10) / 10;
-    console.log(temp1 + "\u00B0" + unit1 + " is " + temp2 + "\u00B0" + unit2 + ".");
-} else if (unit1 === "C") {
-    temp2 = temp1 * 9 / 5 + 32;
-    temp2 = Math.round(temp2 * 10) / 10;
-    console.log(temp1 + "\u00B0" + unit1 + " is " + temp2 + "\u00B0" + unit2 + ".");
-}
+tempConvert(7, "c");
+
 
 // function tempConvert(form) {
 //     var first = form.tempbox.value;
